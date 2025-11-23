9fcef8f51f289d6c94c4dd074f21cebe7230ff9e
landerguevarra01 <guevarralander0@gmail.com>
2025-11-23 14:56:29 +0800

    add desc2

--- app/page.tsx
+++ app/page.tsx
@@ -12,28 +12,28 @@ import SpeakerSection from "./components/Speakers";
 import Timer from "./components/Timer";
 
 export default function Home() {
-  // const router = useRouter();
-  // const timerRef = useRef<NodeJS.Timeout | null>(null);
+  const router = useRouter();
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
 
-  // const goToPosters = () => {
-  //   router.push("/Posters"); // navigate with hash
-  // };
+  const goToPosters = () => {
+    router.push("/Posters"); // navigate with hash
+  };
 
-  // const resetTimer = () => {
-  //   if (timerRef.current) clearTimeout(timerRef.current);
-  //   timerRef.current = setTimeout(goToPosters, 30000); // 20 sec
-  // };
+  const resetTimer = () => {
+    if (timerRef.current) clearTimeout(timerRef.current);
+    timerRef.current = setTimeout(goToPosters, 30000); // 20 sec
+  };
 
-  // useEffect(() => {
-  //   resetTimer();
-  //   const events = ["mousemove", "keydown", "scroll", "touchstart"];
-  //   events.forEach((event) => window.addEventListener(event, resetTimer));
+  useEffect(() => {
+    resetTimer();
+    const events = ["mousemove", "keydown", "scroll", "touchstart"];
+    events.forEach((event) => window.addEventListener(event, resetTimer));
 
-  //   return () => {
-  //     if (timerRef.current) clearTimeout(timerRef.current);
-  //     events.forEach((event) => window.removeEventListener(event, resetTimer));
-  //   };
-  // }, []);
+    return () => {
+      if (timerRef.current) clearTimeout(timerRef.current);
+      events.forEach((event) => window.removeEventListener(event, resetTimer));
+    };
+  }, []);
 
   return (
     <div className="max-w-screen w-full bg-white flex flex-col justify-center items-center">
